c70eeb1850aecd0e2b801a432fb6466de10dc094
SashaMoskalets <59283185+Sasha2811@users.noreply.github.com>
2021-06-12 13:44:16 +0300

    Add files via upload

---
+++ LAB_6/LAB_6/LAB_6/Programs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LAB_6
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Reverse reverse = new Reverse();
+            reverse.NumRevers();
+            reverse.NumRevers(1234);
+            Console.WriteLine(ReverseString());
+            reverse.ReverseStr();
+            Console.WriteLine(reverse.ReverseNumber(123.456));
+            reverse.ReverseNumber();
+            reverse.ReverseWord();
+            reverse.ReverseWord("АБВГ,ДЕЖЗ");
+            Console.WriteLine(reverse.DontReverse("Sasha Moskalets"));
+            reverse.DontReverse();
+            string text = "Hello, my name is Sasha";
+            reverse.DontReverse(ref text);
+            string test = "Text";
+            reverse.DontReverse(test, out string texts);
+
+        }
+        static string ReverseString()
+        {
+            string word = "АБВГ";
+            char[] array = word.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+    }
+}
---
+++ LAB_6/LAB_6/LAB_6/Reverse.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LAB_6
+{
+        public class Reverse
+        {
+            public void NumRevers()
+            {
+                int num = 1234;
+                Console.WriteLine(num.ToString().Reverse().ToArray());
+            }
+            public void NumRevers(int num)
+            {
+                Console.WriteLine(num.ToString().Reverse().ToArray());
+            }
+
+            public string ReverseString(string word)
+            {
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+        public void ReverseStr()
+        {
+            string[] word = {"АБВГ"};
+                Array.Reverse(word);
+                Console.WriteLine(word);
+            }
+            public double ReverseNumber(double number)
+            {
+                return double.Parse(ReverseString(number.ToString()));
+            }
+            public void ReverseNumber()
+            {
+                double number = 123.456;
+                Console.WriteLine(double.Parse(ReverseString(number.ToString())));
+            }
+            public void ReverseWord()
+            {
+                string startWord = "АБВГ,ДЕЖЗ";
+                Console.WriteLine(startWord);
+                string secondWord = "АБВГ,ДЕЖЗ";
+                
+                char[] sReverse = secondWord.ToCharArray();
+                Array.Reverse(sReverse);
+                secondWord = new string(sReverse);
+                Console.WriteLine(secondWord);
+            }
+            public void ReverseWord(string secondWord)
+            {
+                string startWord = "АБВГ,ДЕЖЗ";
+                Console.WriteLine(startWord);
+                
+                char[] sReverse = secondWord.ToCharArray();
+                Array.Reverse(sReverse);
+                secondWord = new string(sReverse);
+                Console.WriteLine(secondWord);
+            }
+            public void DontReverse()
+            {
+                string text = "Hello, my name is Sasha";
+                char[] cArray = text.ToCharArray();
+                string reverse = String.Empty;
+                for (int i = cArray.Length - 1; i > -1; i--)
+                {
+                    reverse += cArray[i];
+                }
+                Console.WriteLine(reverse);
+            }
+            public string DontReverse(string text)
+            {
+                char[] cArray = text.ToCharArray();
+                string reverse = String.Empty;
+                for (int i = cArray.Length - 1; i > -1; i--)
+                {
+                    reverse += cArray[i];
+                }
+                return reverse;
+            }
+            public void DontReverse(ref string text)
+            {
+                char[] cArray = text.ToCharArray();
+                string reverse = String.Empty;
+                for (int i = cArray.Length - 1; i > -1; i--)
+                {
+                    reverse += cArray[i];
+                }
+                Console.WriteLine(reverse);
+            }
+            public void DontReverse(string test, out string texts)
+            {
+                texts = "Sasha";
+                char[] cArray = texts.ToCharArray(); 
+                string reverse = String.Empty;
+                for (int i = cArray.Length - 1; i > -1; i--)
+                {
+                    reverse += cArray[i];
+                }
+                Console.WriteLine(reverse);
+            }
+        }
+    }
+
