0dc1f572e8972cc53febe688bfd388e1574d9826
SashaMoskalets <59283185+Sasha2811@users.noreply.github.com>
2021-06-12 13:41:15 +0300

    Add files via upload

---
+++ LAB_1/LAB_1/LAB_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LAB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, f;
+            Console.Write("Введите x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            f =((((2 * x - 3) * x + 4) * x - 5) * x + 6);
+            Console.Write("F=");
+            Console.WriteLine(f);  
+        }
+    }
+}
