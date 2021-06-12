afee5d393f1633d335812f35436bec2a8a6ef83d
SashaMoskalets <59283185+Sasha2811@users.noreply.github.com>
2021-06-12 13:45:40 +0300

    Add files via upload

---
+++ LAB_7/LAB_7/LAB_7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LABA_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> chList = new List<char>();
+            Console.WriteLine("Вводите символы (Стоп - Enter): ");
+            ConsoleKey n;
+            while ((n = Console.ReadKey(true).Key) != ConsoleKey.Enter)
+            {
+                chList.Add((char)n);
+                Console.Write(n.ToString().ToLower() + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Введите символ, чтоб узнать его индекс: ");
+            char ch = (char)Console.ReadKey().Key;
+            Console.WriteLine();
+            Console.WriteLine("Индекс символа: ");
+            for (int i = 0; i < chList.Count; i++) 
+                if (chList[i] == ch)
+                Console.Write(i + " ");
+            char[] arr = chList.ToArray();
+            Console.ReadKey(true);
+        }
+    }
+}
