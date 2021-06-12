6d498e72e4bf7a032b3699a1dcb625ec5eeec122
SashaMoskalets <59283185+Sasha2811@users.noreply.github.com>
2021-06-12 13:42:09 +0300

    Add files via upload

---
+++ LAB_3/LABA_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LABA_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трёхзначное число: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Вы ввели такое число: ");
+            Console.WriteLine(num);
+            if (num >= 100)
+            {
+
+                if (num / 100 + num % 10 == 2 * (num / 10) % 10)
+                    Console.Write("Правильное");
+
+                else
+                    Console.Write("Неправильно");
+
+            }
+            else
+            Console.Write("Неправильное число, введите трехзначное число!!!");
+            Console.ReadLine();
+        }
+    }
+}
+
